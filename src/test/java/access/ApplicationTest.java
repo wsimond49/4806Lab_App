d76aa6f2dc088105fc72c2e78c97a86081d971b5
simondewilde <simondewilde@LABS.SCE.CARLETON.CA>
2020-02-05 14:27:47 -0500

    Add .travis.yml

--- src/test/java/access/ApplicationTest.java
+++ src/test/java/access/ApplicationTest.java
@@ -1,12 +1,10 @@
 package access;
 
-import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,7 +21,12 @@ public class ApplicationTest {
     private MockMvc mockMvc;
 
     @Test
-    public void shouldReturnDefaultMessage() throws Exception {
+    public void addressBookOk() throws Exception {
         this.mockMvc.perform(MockMvcRequestBuilders.get("/address/show?id=1")).andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void buddyInfoOk() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/buddy/show?id=1")).andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
